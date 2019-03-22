6bcfa9f2397502638939e56964823141b7e19ff5
Amit Kharka <kharkaa@hcl.com>
2019-03-22 08:23:28 +0530

    WIP #junk-code-setup-amit [tooltip component added]

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Link from './components/Link';
 import Breadcrumb from './components/Breadcrumb';
 import Tag from './components/Tag';
 import Heading from './components/Heading';
+import Tooltip from './components/Tooltip';
 
 class App extends Component<{}> {
     render() {
@@ -58,6 +59,53 @@ class App extends Component<{}> {
                     >
                         La La La
                     </Heading>
+                </div>
+                {/* Tooltip */}
+                <div>
+                    Contrary to popular belief, Lorem Ipsum is not simply random text.
+                    It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old.
+                    &nbsp;
+                    <Tooltip
+                        title="Richard McClintock"
+                        className="tooltip"
+                        clickToOpen={true}
+                        direction="top"
+                    >
+                        <b><i>Richard McClintock</i></b>
+                    </Tooltip>
+                    , a Latin professor at Hampden-Sydney College in &nbsp;
+
+                    <Tooltip
+                        title="Virginia"
+                        className="tooltip"
+                        clickToOpen={true}
+                        direction="bottom"
+                    >
+                        <b><i>Virginia</i></b>
+                    </Tooltip>,
+                    looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage,
+                    and going through the cites of the word in classical literature, discovered the undoubtable source.
+                    Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of
+                    <Tooltip
+                        title="de Finibus Bonorum et Malorum"
+                        className="tooltip"
+                        clickToOpen={true}
+                        direction="right"
+                    >
+                        <b><i>"de Finibus Bonorum et Malorum"</i></b>
+                    </Tooltip>
+                    (The Extremes of Good and Evil) by &nbsp;
+                    <Tooltip
+                        title="Cicero"
+                        className="tooltip"
+                        clickToOpen={true}
+                        direction="left"
+                    >
+                        <b><i>Cicero</i></b>
+                    </Tooltip>
+                    , written in 45 BC.
+                    This book is a treatise on the theory of ethics, very popular during the Renaissance.
+                    The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
                 </div>
             </main>
         );
---
+++ src/components/Tooltip/Tooltip.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './Tooltip.scss';
+
+type Props = {
+    className: string,
+    clickToOpen?: boolean,
+    children: string,
+    direction?: string,
+    title: string
+};
+
+function TooltipContainer(title: string, myRef: any) {
+    return ReactDOM.createPortal(
+        (
+            <div className="tooltiptext" ref={myRef}>
+                {title}
+            </div>
+        ),
+        document.body
+    )
+}
+
+
+export default class Tooltip extends Component {
+    state = {
+        displayTooltip: false,
+    };
+
+    myRef = React.createRef();
+
+    _setTooltipPosition = ({ tooltipMetaData, tooltipTextMetaData }) => {
+        let direction = this.props.direction || 'auto';
+        let top = 0,
+            left = 0;
+        for (let i = 0; i < 2; i++) {
+            switch (direction) {
+                case 'auto':
+                case 'top': {
+                    if ((tooltipMetaData.top + 20) > tooltipTextMetaData.height &&
+                        tooltipMetaData.left > (tooltipTextMetaData.width / 2 - 30) &&
+                        (window.innerWidth - tooltipMetaData.left - tooltipMetaData.width) > (tooltipTextMetaData.width / 2 - 30)) {
+                        top = tooltipMetaData.top - tooltipTextMetaData.height - 10;
+                        left = (tooltipMetaData.left + (tooltipMetaData.width / 2) - (tooltipTextMetaData.width / 2)) + 10;
+                        this.myRef.current.setAttribute('data-tooltip-direction', 'top');
+                        break;
+                    }
+                }
+                case 'bottom': {
+                    if ((tooltipMetaData.bottom + 20) > tooltipTextMetaData.height &&
+                        tooltipMetaData.left > (tooltipTextMetaData.width / 2 - 30) &&
+                        (window.innerWidth - tooltipMetaData.left - tooltipMetaData.width) > (tooltipTextMetaData.width / 2 - 30)) {
+                        top = tooltipMetaData.bottom + 10;
+                        left = (tooltipMetaData.left + (tooltipMetaData.width / 2) - (tooltipTextMetaData.width / 2)) + 10;
+                        this.myRef.current.setAttribute('data-tooltip-direction', 'bottom');
+                        break;
+                    }
+                }
+                case 'left': {
+                    if (tooltipMetaData.left > (tooltipTextMetaData.width + 20) &&
+                        tooltipMetaData.top > (tooltipTextMetaData.height / 2) &&
+                        tooltipMetaData.bottom > (tooltipTextMetaData.height / 2)) {
+                        top = tooltipMetaData.top - (tooltipTextMetaData.height / 2) + 10;
+                        left = tooltipMetaData.left - tooltipTextMetaData.width - 10;
+                        this.myRef.current.setAttribute('data-tooltip-direction', 'left');
+                        break;
+                    }
+                }
+                case 'right': {
+                    if ((window.innerWidth - tooltipMetaData.left - tooltipMetaData.width) > (tooltipTextMetaData.width + 20) &&
+                        tooltipMetaData.top > (tooltipTextMetaData.height / 2) &&
+                        tooltipMetaData.bottom > (tooltipTextMetaData.height / 2)) {
+                        top = tooltipMetaData.top - (tooltipTextMetaData.height / 2) + 10;
+                        left = tooltipMetaData.left + tooltipMetaData.width + 10;
+                        this.myRef.current.setAttribute('data-tooltip-direction', 'right');
+                        break;
+                    }
+                }
+                default:
+                    direction = 'auto';
+            }
+        }
+
+        this.myRef.current.style.left = `${left}px`;
+        this.myRef.current.style.top = `${top}px`;
+    }
+
+    _handleMouseEnter = event => {
+        const tooltipMetaData = event.currentTarget.getBoundingClientRect();
+        this.setState({ displayTooltip: true }, () => {
+            const tooltipTextMetaData = this.myRef.current.getClientRects()[0];
+            this._setTooltipPosition({ tooltipMetaData, tooltipTextMetaData });
+        });
+    }
+
+    _handleMouseLeave = event => {
+        this.myRef.current.removeAttribute('data-tooltip-direction');
+        this.setState({ displayTooltip: false });
+    }
+
+    render() {
+        return (
+            <div className="tooltip" onMouseEnter={this._handleMouseEnter} onMouseLeave={this._handleMouseLeave}>
+                {this.props.children}
+                {this.state.displayTooltip ? TooltipContainer(this.props.title, this.myRef) : null}
+            </div >
+
+        )
+    }
+};
