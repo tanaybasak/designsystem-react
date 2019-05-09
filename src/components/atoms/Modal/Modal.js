19e59d2293a713f51b18d21dd8cd382090834c14
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-09 20:17:46 +0530

    feature-UICOE-215: First Commit

---
+++ src/components/atoms/Modal/Modal.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import { prefix } from '../../../settings';
+
+export default function Modal() {
+
+    return null
+       
+};
+
+Modal.propTypes = {
+   
+};
+
+Modal.defaultProps = {
+   
+};
