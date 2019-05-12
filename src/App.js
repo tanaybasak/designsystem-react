53b5fae59fce8655cd71ac7e2089549fb8871153
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-05-12 18:02:06 +0530

    Content will be passesd as node

--- src/App.js
+++ src/App.js
@@ -213,15 +213,31 @@ class App extends Component {
                     </div>
                     <div className="col-12 mt-5">
                         {/* Danger type Modals */}
-                        <Modal type="danger" label="optional label" heading="heading" content="This is temporaray content" footer onClose={event => { }} onDelete={event => { }}></Modal>
-                        <Modal type="danger" label="optional label" heading="heading" content="This is temporaray content" onClose={event => { }}></Modal>
-                        <Modal type="danger" heading="heading" content="This is temporaray content" footer onClose={event => { }} onDelete={event => { }}></Modal>
-                        <Modal type="danger" heading="heading" content="This is temporaray content" onClose={event => { }}></Modal>
+                        <Modal type="danger" label="optional label" heading="heading" footer onClose={event => { }} onDelete={event => { }}>
+                          <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal type="danger" label="optional label" heading="heading" onClose={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal type="danger" heading="heading" footer onClose={event => { }} onDelete={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal type="danger" heading="heading" onClose={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
                         {/* Default type Modals */}
-                        <Modal label="optional label" heading="heading" content="This is temporaray content" footer onClose={event => { }} onSave={event => { }}></Modal>
-                        <Modal label="optional label" heading="heading" content="This is temporaray content" onClose={event => { }}></Modal>
-                        <Modal heading="heading" content="This is temporaray content" footer onClose={event => { }} onSave={event => { }}></Modal>
-                        <Modal heading="heading" content="This is temporaray content" onClose={event => { }}></Modal>
+                        <Modal label="optional label" heading="heading" footer onClose={event => { }} onSave={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal label="optional label" heading="heading" onClose={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal heading="heading" footer onClose={event => { }} onSave={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
+                        <Modal heading="heading" onClose={event => { }}>
+                         <Paragraph> this is temporaray content</Paragraph>
+                        </Modal>
                     </div>
                 </div>
             </main >
