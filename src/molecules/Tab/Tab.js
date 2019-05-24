c41df21020434cb1395e8423460f283c8d2ccddb
Vivek Ananth <vivekananth@Viveks-MacBook-Pro.local>
2019-05-24 14:54:21 +0530

    Tab changes for 255

--- src/molecules/Tab/Tab.js
+++ src/molecules/Tab/Tab.js
@@ -2,31 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import prefix from '../../settings';
 
-function Tab({ label, onClick, active, isDisabled }) {
-  return (
-    <li
-      role='tab'
-      className={`${prefix}-tabs-nav-item ${active ? 'active' : ''} ${
-        isDisabled ? `${prefix}-tabs-disabled` : ''
-      }`}
-      onClick={onClick}
-    >
-      <a className={`${prefix}-tabs-nav-link`} href={()=>{}}>{label}</a>
-    </li>
-  );
+function Tab({ label, onClick, active = false, isDisabled }) {
+    return (
+        <li
+            role='tab'
+            className={`${prefix}-tabs-nav-item ${active ? 'active' : ''} ${
+                isDisabled ? `${prefix}-tabs-disabled` : ''
+                }`}
+            onClick={onClick}
+        >
+            <a className={`${prefix}-tabs-nav-link`} href={() => { }} tabIndex={0}>{label}</a>
+        </li>
+    );
 }
 
 Tab.propTypes = {
-  label: PropTypes.string,
-  isDisabled: PropTypes.bool,
-  onClick: PropTypes.func,
-  active : PropTypes.bool
+    label: PropTypes.string,
+    isDisabled: PropTypes.bool,
+    onClick: PropTypes.func,
 };
 Tab.defaultProps = {
-  label: '',
-  isDisabled: false,
-  onClick: () => {},
-  active : false
+    label: '',
+    isDisabled: false,
+    onClick: () => { }
 };
 
 export default Tab;
