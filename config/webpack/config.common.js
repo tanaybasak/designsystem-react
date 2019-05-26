857e52d0ae3c5d31af2e7aa1897ee63a8992ccf7
Amit Kharka <kharkaa@hcl.com>
2019-05-27 00:34:16 +0530

    Used patron-react-starter boilerplate

---
+++ config/webpack/config.common.js
@@ -0,0 +1,116 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const HTML_TEMPLATE = path.resolve('./index.html');
+const FAVICON_PATH = path.resolve('./assets/images/favicon.ico');
+const NODE_MODULES_DIR = path.resolve('./node_modules');
+const SRC_DIR = path.resolve('./src');
+const DIST_DIR = path.resolve('./dist');
+
+module.exports.DIRS = {
+    HTML_TEMPLATE,
+    FAVICON_PATH,
+    NODE_MODULES_DIR,
+    SRC_DIR,
+    DIST_DIR
+};
+
+module.exports.commonConfig = {
+    context: SRC_DIR,
+    entry: [
+        'core-js/stable',
+        'regenerator-runtime/runtime',
+        `${SRC_DIR}/index.js`
+    ],
+    output: {
+        filename: 'js/[name].[hash:8].js',
+        chunkFilename: 'js/[name].[chunkhash:8].js',
+        path: DIST_DIR,
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                query: {
+                    presets: [
+                        [
+                            '@babel/preset-env',
+                            {
+                                modules: false,
+                                useBuiltIns: 'entry',
+                                corejs: {
+                                    version: 3,
+                                    proposals: true
+                                }
+                            }
+                        ],
+                        '@babel/preset-react'
+                    ]
+                }
+            },
+            {
+                test: /\.(le|sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        loader: 'css-loader'
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options: {
+                            ident: 'postcss',
+                            plugins: () => [
+                                require('postcss-preset-env')({ browsers: 'last 2 versions' })
+                            ]
+                        }
+                    },
+                    {
+                        loader: 'sass-loader'
+                    }
+                ]
+            },
+            {
+                test: /\.(png|svg|jpe?g|gif|woff2?|eot|[ot]tf)$/,
+                use: ['file-loader']
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: ['csv-loader']
+            },
+            {
+                test: /\.xml$/,
+                use: ['xml-loader']
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: HTML_TEMPLATE,
+            filename: 'index.html',
+            favicon: FAVICON_PATH
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'css/[name].[chunkhash:8].css',
+            chunkFilename: 'css/[name].[chunkhash:8].css'
+        })
+    ],
+    resolve: {
+        alias: {
+            'patron-css': path.resolve(`${NODE_MODULES_DIR}/patron-css/dist/patron-style.css`)
+        },
+        extensions: ['.wasm', '.mjs', '.js', '.jsx', '.json'],
+        modules: [SRC_DIR, NODE_MODULES_DIR]
+    },
+    performance: {
+        hints: 'warning'
+    },
+    stats: {
+        warnings: false
+    },
+    bail: true
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'patron-css';
 import './index.scss';
-import App from './App';
-import 'patron-css-build/dist/patron-style.css';
 
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+);
