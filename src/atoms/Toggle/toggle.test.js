762b50e270bd4db0fdaead07e15849bbfddcc0e3
Vivek Ananth <vivekananth@Viveks-MacBook-Pro.local>
2019-06-04 15:04:38 +0530

    toggle changes

--- src/atoms/Toggle/toggle.test.js
+++ src/atoms/Toggle/toggle.test.js
@@ -1,22 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { mount } from 'enzyme';
-import prefix from '../../settings';
+import { shallow } from 'enzyme';
 import Toggle from './Toggle';
 
 describe('<Toggle> component', () => {
     it('snapshots/renders Toggle correctly', () => {
-        const tree = renderer.create(<Toggle id="hellotoggle"/>).toJSON();
+        const tree = renderer.create(<Toggle id="hellotoggle" />).toJSON();
         expect(tree).toMatchSnapshot();
     });
 
-    // it('renders checkobx with label', () => {
-    //     const checkbox = mount((<Toggle labelText='defaultcheckbox' />));
-    //     expect(checkbox.text()).toBe('defaultcheckbox');
-    // });
-
-    // it('renders checkobx with default classname', () => {
-    //     const checkbox = mount((<Toggle />));
-    //     expect(checkbox.find(`.${prefix}-checkbox`).hasClass('hcl-checkbox')).toBe(true);
-    // });
+    it('simulate default change event in toggle', () => {
+        const wrapper = shallow(<Toggle id="hellotoggle" />);
+        expect(wrapper.find('input[type="checkbox"]').props().checked).toBeFalsy();
+        wrapper.find('input[type="checkbox"]').simulate('change', { target: {} });
+        expect(wrapper.find('input[type="checkbox"]').props().checked).toBeTruthy();
+    });
 });
