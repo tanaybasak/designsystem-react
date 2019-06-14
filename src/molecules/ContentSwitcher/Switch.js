645e9394f8bca2de8032d7419aa0ef1b2e84ab2c
Vivek Ananth <vivekananth@Viveks-MacBook-Pro.local>
2019-06-14 12:05:02 +0530

    switch changes

--- src/molecules/ContentSwitcher/Switch.js
+++ src/molecules/ContentSwitcher/Switch.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import Button from '../../atoms/Button';
 import prefix from '../../settings';
 
 function Switch({ label, onClick, active, isDisabled, iconClass, ...restProps }) {
