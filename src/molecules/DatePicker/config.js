c312f559492cfa51115169ad051244b6b36fbb08
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-06-14 13:37:44 +0530

    config.js deleted

--- src/molecules/DatePicker/config.js
+++
@@ -1,4 +0,0 @@
-export const PREFIX = "hcl";
-export const VERSION = "0.0.1";
-export const WEEKDAYS = ['S', 'M', 'T', 'W', 'Th', 'F', 'S'];
-export const MONTHS = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
