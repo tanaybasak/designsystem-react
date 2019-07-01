fc875ad2e922dea3c3a1b3184d99186cefb54d6e
Vivek Ananth <vivekananth@Viveks-MacBook-Pro.local>
2019-07-01 16:02:45 +0530

    pagination changes

--- src/atoms/Pagination/Pager.js
+++ src/atoms/Pagination/Pager.js
@@ -1,25 +1,22 @@
 import React from 'react';
-//import PropTypes from 'prop-types';
 import prefix from '../../settings';
 
 const Pager = (props, ref) => {
-    // eslint-disable-next-line react/prop-types
     if (props.options.length <= 0) {
         return null;
     }
 
     return (
         <>
-            {/* eslint-disable-next-line react/prop-types */}
             <select className={props.className}
                 ref={ref}
                 onChange={(e) => {
-                    // eslint-disable-next-line react/prop-types
+
                     props.onChange(ref.current.value, e);
                 }}
             >
                 {
-                    // eslint-disable-next-line react/prop-types
+
                     props.options.map((item, idx) => {
                         return (
                             <option key={idx} value={item}>{item}</option>
@@ -33,17 +30,5 @@ const Pager = (props, ref) => {
         </>
     )
 };
-
-// Pager.protoTypes = {
-//     options: PropTypes.arrayOf(PropTypes.number),
-//     className: PropTypes.string,
-//     onChange: PropTypes.func
-// }
-
-// Pager.defaultProps = {
-//     options: [],
-//     className: '',
-//     onChange: () => { }
-// }
 
 export default React.forwardRef(Pager);
--- src/atoms/Pagination/Pagination.js
+++ src/atoms/Pagination/Pagination.js
@@ -20,26 +20,20 @@ const Pagination = ({ totalItems, pageSizes, itemsPerPageText }) => {
     const totalpagesdisplayRef = useRef();
 
     //useEffect PagesItems
-    useEffect(
-        () => {
-            rangeStartRef.current.innerHTML = 1;
-            rangeEndRef.current.innerHTML = items;
-            calculatePages();
-        },
-        [items, pageItemsSelectedRef.current]
-    )
+    useEffect(() => {
+        rangeStartRef.current.innerHTML = 1;
+        rangeEndRef.current.innerHTML = items;
+        calculatePages();
+    }, [items, pageItemsSelectedRef.current])
 
     // useEffect pages
-    useEffect(
-        () => {
-            if (pagesRef.current && pagesRef.current.selectedIndex != -1) {
-                pagesRef.current.selectedIndex = 0;
-                startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
-                toggleNavButtons();
-            }
-        },
-        [pageItems, pagesRef.current]
-    )
+    useEffect(() => {
+        if (pagesRef.current && pagesRef.current.selectedIndex != -1) {
+            pagesRef.current.selectedIndex = 0;
+            startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
+            toggleNavButtons();
+        }
+    }, [pageItems, pagesRef.current])
 
     const calculatePages = () => {
         if (pageItemsSelectedRef.current && totalItems) {
@@ -74,111 +68,115 @@ const Pagination = ({ totalItems, pageSizes, itemsPerPageText }) => {
     }
 
     const _onItemsChange = () => {
-        setItems(pageItemsSelectedRef.current.options[pageItemsSelectedRef.current.selectedIndex].value);
+        if (pageItemsSelectedRef.current) {
+            setItems(pageItemsSelectedRef.current.options[pageItemsSelectedRef.current.selectedIndex].value);
+        }
     }
 
     const adjustRange = () => {
-        let pageSize = pageItemsSelectedRef.current.options[pageItemsSelectedRef.current.selectedIndex],
-            pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
+        if (pageItemsSelectedRef.current && pagesRef.current) {
+            let pageSize = pageItemsSelectedRef.current.options[pageItemsSelectedRef.current.selectedIndex],
+                pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
 
-        let rangeStart = rangeStartRef.current,
-            rangeEnd = rangeEndRef.current;
-        if (rangeStart && rangeEnd) {
-            rangeStart.innerHTML = ((pageDropDown.value - 1) * pageSize.value) + 1;
-            if ((pageDropDown.value * pageSize.value) > totalItems) {
-                rangeEnd.innerHTML = totalItems;
-            } else {
-                rangeEnd.innerHTML = (pageDropDown.value * pageSize.value);
+            let rangeStart = rangeStartRef.current,
+                rangeEnd = rangeEndRef.current;
+            if (rangeStart && rangeEnd) {
+                rangeStart.innerHTML = ((pageDropDown.value - 1) * pageSize.value) + 1;
+                if ((pageDropDown.value * pageSize.value) > totalItems) {
+                    rangeEnd.innerHTML = totalItems;
+                } else {
+                    rangeEnd.innerHTML = (pageDropDown.value * pageSize.value);
+                }
             }
         }
     }
 
     const _onNextClick = (e) => {
         e.preventDefault();
-        let pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
-        if (nextbtnRef.current && !nextbtnRef.current.disabled && totalPages != pageDropDown.value) {
-            pagesRef.current.selectedIndex++;
-            startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
-            toggleNavButtons();
-            adjustRange();
+        if (pagesRef.current) {
+            let pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
+            if (nextbtnRef.current && !nextbtnRef.current.disabled && totalPages != pageDropDown.value) {
+                pagesRef.current.selectedIndex++;
+                startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
+                toggleNavButtons();
+                adjustRange();
+            }
         }
     }
 
     const _onPreviousClick = (e) => {
         e.preventDefault();
-        let pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
-        if (previousbtnRef.current && !previousbtnRef.current.disabled && pageDropDown.value > 1) {
-            pagesRef.current.selectedIndex--;
-            startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
-            toggleNavButtons();
-            adjustRange();
+        if (pagesRef.current) {
+            let pageDropDown = pagesRef.current.options[pagesRef.current.selectedIndex];
+            if (previousbtnRef.current && !previousbtnRef.current.disabled && pageDropDown.value > 1) {
+                pagesRef.current.selectedIndex--;
+                startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
+                toggleNavButtons();
+                adjustRange();
+            }
         }
     }
 
     const _onPagesChange = () => {
         toggleNavButtons();
         adjustRange();
-        startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
+        if (startpagedisplayRef.current) {
+            startpagedisplayRef.current.innerHTML = pagesRef.current.options[pagesRef.current.selectedIndex].value;
+        }
     }
 
     return (
-        <>
-            <div className={`${prefix}-pagination`}>
-                <div className={`${prefix}-pagination-left`}>
-                    <label
-                        id={`${prefix}-pagination-select-7`}
-                        className={`${prefix}-pagination-text`}
-                        htmlFor={`${prefix}-pagination-select-7`}
-                    >
-                        {itemsPerPageText}
-                    </label>
-                    <div className={`${prefix}-pagination-select-wrapper`}>
-                        <Pager ref={pageItemsSelectedRef} onChange={_onItemsChange} options={pageSizes} className={`${prefix}-pagination-select ${prefix}-page-items`} />
-                    </div>
-                    <span className={`${prefix}-pagination-text`}>
-                        <span className={`${prefix}-pagination-range`}>
-                            <span className={`${prefix}-range-start`} ref={rangeStartRef} />&nbsp;
+        <div className={`${prefix}-pagination`}>
+            <div className={`${prefix}-pagination-left`}>
+                <span>
+                    {itemsPerPageText}
+                </span>
+                <div className={`${prefix}-pagination-select-wrapper`}>
+                    <Pager ref={pageItemsSelectedRef} onChange={_onItemsChange} options={pageSizes} className={`${prefix}-pagination-select ${prefix}-page-items`} />
+                </div>
+                <span className={`${prefix}-pagination-text`}>
+                    <span className={`${prefix}-pagination-range`}>
+                        <span className={`${prefix}-range-start`} ref={rangeStartRef} />&nbsp;
                              <span className={`${prefix}-range-separator`}>-</span>&nbsp;
                              <span className={`${prefix}-range-end`} ref={rangeEndRef} />
-                        </span>&nbsp;of&nbsp;
+                    </span>&nbsp;of&nbsp;
                          <span className={`${prefix}-pagination-totalitems`}>
-                            {totalItems}
-                        </span>&nbsp;items
+                        {totalItems}
+                    </span>&nbsp;items
                      </span>
-                </div>
-                <div className={`${prefix}-pagination-right`}>
-                    <span className={`${prefix}-pagination-text`}>
-                        <span className={`${prefix}-page-start`} ref={startpagedisplayRef}>
-                            1
+            </div>
+            <div className={`${prefix}-pagination-right`}>
+                <span className={`${prefix}-pagination-text`}>
+                    <span className={`${prefix}-page-start`} ref={startpagedisplayRef}>
+                        1
                         </span>&nbsp;of&nbsp;
                          <span className={`${prefix}-page-end`} ref={totalpagesdisplayRef} />{totalPages}&nbsp;pages
                      </span>
-                    <button className={`${prefix}-pagination-button-previous`}
-                        aria-label={`Previous page`}
-                        ref={previousbtnRef}
-                        onClick={(e) => { _onPreviousClick(e); }}
-                    >
-                        <svg className={`${prefix}-pagination-button-icon`} width="7" height="12" viewBox="0 0 7 12">
-                            <path fillRule="nonzero" d="M1.45 6.002L7 11.27l-.685.726L0 6.003 6.315 0 7 .726z" />
-                        </svg>
-                    </button>
-                    <div className={`${prefix}-pagination-select-wrapper`}>
-                        <Pager ref={pagesRef} onChange={_onPagesChange} options={pageItems} className={`${prefix}-pagination-select ${prefix}-page-number`} />
-                    </div>
-                    <button className={`${prefix}-pagination-button-next`}
-                        aria-label="Next page"
-                        ref={nextbtnRef}
-                        onClick={(e) => { _onNextClick(e); }}
-                    >
-                        <svg className={`${prefix}-pagination-button-icon`} width="7" height="12" viewBox="0 0 7 12">
-                            <path fillRule="nonzero"
-                                d="M5.569 5.994L0 .726.687 0l6.336 5.994-6.335 6.002L0 11.27z"
-                            />
-                        </svg>
-                    </button>
+                <button className={`${prefix}-pagination-button-previous`}
+                    aria-label={`Previous page`}
+                    ref={previousbtnRef}
+                    onClick={(e) => { _onPreviousClick(e); }}
+                >
+                    <svg className={`${prefix}-pagination-button-icon`} width="7" height="12" viewBox="0 0 7 12">
+                        <path fillRule="nonzero" d="M1.45 6.002L7 11.27l-.685.726L0 6.003 6.315 0 7 .726z" />
+                    </svg>
+                </button>
+                <div className={`${prefix}-pagination-select-wrapper`}>
+                    <Pager ref={pagesRef} onChange={_onPagesChange} options={pageItems} className={`${prefix}-pagination-select ${prefix}-page-number`} />
                 </div>
+                <button className={`${prefix}-pagination-button-next`}
+                    aria-label="Next page"
+                    ref={nextbtnRef}
+                    onClick={(e) => { _onNextClick(e); }}
+                >
+                    <svg className={`${prefix}-pagination-button-icon`} width="7" height="12" viewBox="0 0 7 12">
+                        <path fillRule="nonzero"
+                            d="M5.569 5.994L0 .726.687 0l6.336 5.994-6.335 6.002L0 11.27z"
+                        />
+                    </svg>
+                </button>
             </div>
-        </>
+        </div>
     )
 }
 
