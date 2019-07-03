b59fe8868b9dabcdfa0b8ca7afc4ecd93642080b
Mayank Gupta <mayankgupta@Mayanks-MacBook-Pro.local>
2019-07-03 17:06:31 +0530

    setTypeState removed

--- src/atoms/Dropdown/Dropdown.js
+++ src/atoms/Dropdown/Dropdown.js
@@ -6,13 +6,12 @@ import { trackDocumentClick, positionComponent } from '../../util/utility'
 
 const Dropdown = ({ type, items, label, onChange, selectedIndex }) => {
     const [isOpen, setIsOpen] = useState(false);
-    const [selected, setSelected] = useState(selectedIndex ? items[selectedIndex] : null);
+    const [selected, setSelected] = useState(selectedIndex + 1 ? items[selectedIndex] : null);
     const [typeState, setTypeState] = useState(type);
     const dropDown = useRef(null);
 
     useEffect(() => {
          setSelected(selectedIndex ? items[selectedIndex] : null);
-         setTypeState(type);
          positionComponent(() => { setTypeState('top') }, () => { setTypeState('bottom') }, type, dropDown.current.getElementsByTagName('ul')[0]);
      });
 
