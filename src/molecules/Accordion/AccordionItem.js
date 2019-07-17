4f6906762751e1db52914b12a849c6b4906c9c9b
Vatsal Bhardwaj <vatsalbhardwaj@Vatsals-MacBook-Pro.local>
2019-07-17 13:37:00 +0530

    FIX #UICOE-403 [Fixed issue in Accordion]

--- src/molecules/Accordion/AccordionItem.js
+++ src/molecules/Accordion/AccordionItem.js
@@ -2,49 +2,53 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import prefix from '../../settings';
 
-const AccordionItem = ({ title, expanded, onChange, onExpand, className, children, dataIndex, ...restProps }) => {
+export default function AccordionItem({
+  title,
+  expanded,
+  onChange,
+  onExpand,
+  className,
+  children,
+  dataIndex,
+  ...restProps
+}) {
+  const classnames = `${prefix}-accordion-item ${className} ${
+    expanded ? 'expanded' : ''
+  }`.trim();
 
-    const classnames = `${prefix}-accordion-item ${className}`.trim();
-
-    return (
-        <li className={`${classnames}${expanded ? ' expanded' : ''}`} {...restProps}>
-            <div
-                className={`${prefix}-accordion-title`}
-                data-index={dataIndex}
-                onClick={
-                    event => {
-                        onChange(event);
-                        onExpand(event);
-                    }
-                }
-            >
-                {title}
-            </div>
-            <div className={`${prefix}-accordion-content`}>
-                {children}
-            </div>
-        </li>
-    );
-};
+  return (
+    <li className={classnames} {...restProps}>
+      <div
+        className={`${prefix}-accordion-title`}
+        data-index={dataIndex}
+        onClick={event => {
+          onChange(event);
+          onExpand(event);
+        }}
+      >
+        {title}
+      </div>
+      <div className={`${prefix}-accordion-content`}>{children}</div>
+    </li>
+  );
+}
 
 AccordionItem.propTypes = {
-    title: PropTypes.string.isRequired,
-    expanded: PropTypes.bool,
-    onChange: PropTypes.func,
-    onExpand: PropTypes.func,
-    className: PropTypes.string,
-    children: PropTypes.any,
-    dataIndex: PropTypes.number
+  title: PropTypes.string.isRequired,
+  expanded: PropTypes.bool,
+  onChange: PropTypes.func,
+  onExpand: PropTypes.func,
+  className: PropTypes.string,
+  children: PropTypes.any,
+  dataIndex: PropTypes.number
 };
 
 AccordionItem.defaultProps = {
-    title: null,
-    expanded: false,
-    onChange: () => { },
-    onExpand: () => { },
-    className: '',
-    children: '',
-    dataIndex: null
+  title: null,
+  expanded: false,
+  onChange: () => {},
+  onExpand: () => {},
+  className: '',
+  children: '',
+  dataIndex: null
 };
-
-export default AccordionItem;
