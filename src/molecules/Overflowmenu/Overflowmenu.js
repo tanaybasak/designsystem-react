9211285febbbe215d646cb2af3ced24b088d57fe
Delita <delitajosna.dsouza@hcl.com>
2020-03-19 12:07:57 +0530

    overflow menu click out issue fixed

--- src/molecules/Overflowmenu/Overflowmenu.js
+++ src/molecules/Overflowmenu/Overflowmenu.js
@@ -1,20 +1,22 @@
 import React, { useState, useEffect, useRef } from 'react';
 import PropTypes from 'prop-types';
 import MenuList from '../../atoms/MenuList';
+import { addListener, removeListeners } from '../../util/eventManager';
 import prefix from '../../settings';
-import { trackDocumentClick } from '../../util/utility';
 
+let overflowIdRef = 0;
 const Overflowmenu = ({
   className,
   direction,
   children,
   ellipsisType,
-  onClick,
+  onClicked,
   listItems,
   ...restProps
 }) => {
   const [display, changeDisplay] = useState(false);
   const overflow = useRef(null);
+  const [overflowId] = useState(overflowIdRef++);
 
   useEffect(() => {
     const overflowMenu = overflow.current.children[1];
@@ -27,7 +29,7 @@ const Overflowmenu = ({
         8 -
         parseInt(getComputedStyle(icon).marginBottom)
       ).toString();
-      
+
       overflowMenu.style.display = 'block';
       updateOverflowMenuPos(overflowMenu, icon, caret, outOfBound);
       if (!isInViewport(overflowMenu)) {
@@ -39,6 +41,34 @@ const Overflowmenu = ({
     }
   });
 
+  useEffect(() => {
+    if (!display) {
+      removeListeners('overflow-' + overflowId, 'click');
+    } else {
+      addListener(
+        'overflow-' + overflowId,
+        'click',
+        e => {
+          handleClick(e);
+        },
+        true
+      );
+    }
+  }, [display]);
+
+  const handleClick = e => {
+    if (overflow.current) {
+      if (e && overflow.current.contains(e.target)) {
+        return;
+      }
+      changeDisplay(false);
+    }
+  };
+
+  const clickHandler = () => {
+    changeDisplay(!display);
+  };
+
   const isInViewport = elem => {
     const bounding = elem.getBoundingClientRect();
     return (
@@ -46,19 +76,6 @@ const Overflowmenu = ({
       bounding.right <=
         (window.innerWidth || document.documentElement.clientWidth)
     );
-  };
-
-  const clickHandler = event => {
-    if (onClick) {
-      onClick(event);
-    }
-    event.stopPropagation();
-    changeDisplay(!display);
-    trackDocumentClick(overflow.current, () => {
-      if (changeDisplay(false)) {
-        changeDisplay(display);
-      }
-    });
   };
 
   const updateOverflowMenuPos = (overflowMenu, icon, caret, outOfBound) => {
@@ -183,9 +200,9 @@ const Overflowmenu = ({
         >
           <MenuList
             items={listItems}
-            onClick={event => {
+            onSelect={event => {
               changeDisplay(false);
-              onClick(event);
+              onClicked(event);
             }}
           />
           <div
@@ -204,7 +221,7 @@ Overflowmenu.defaultProps = {
   listItems: null,
   children: null,
   ellipsisType: 'vertical',
-  onClick: () => {},
+  onClicked: () => {},
   className: ''
 };
 
@@ -223,7 +240,7 @@ Overflowmenu.propTypes = {
   ellipsisType: PropTypes.oneOf(['vertical', 'horizontal']),
 
   /** Callback function on selecting item*/
-  onClick: PropTypes.func.isRequired,
+  onClicked: PropTypes.func.isRequired,
 
   /** Class/clasess will be applied on the parent div of OverflowMenu */
   className: PropTypes.string
