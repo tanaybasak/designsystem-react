67816d5d6bfc7fa520245fbbbd82439da586f6da
Delita <delitajosna.dsouza@hcl.com>
2020-03-20 12:17:49 +0530

    Keyboard accessibility for content switcher

--- src/molecules/ContentSwitcher/ContentSwitcher.js
+++ src/molecules/ContentSwitcher/ContentSwitcher.js
@@ -2,6 +2,37 @@ import React, { useState, cloneElement } from 'react';
 import PropTypes from 'prop-types';
 import prefix from '../../settings';
 
+const keyDownOnContextSwitch = e => {
+    const key = e.which || e.keyCode;
+    const nodeElement = e.currentTarget;
+    
+    switch (key) {
+      case 39: {
+        if (!nodeElement.nextElementSibling) {
+          nodeElement.parentElement.firstElementChild.focus();
+        } else if (nodeElement.nextElementSibling.disabled === true) {
+          nodeElement.nextElementSibling.nextElementSibling.focus();
+        } else {
+          nodeElement.nextElementSibling.focus();
+        }
+        e.preventDefault();
+        break;
+      }
+      case 37: {
+        if (!nodeElement.previousElementSibling) {
+          nodeElement.parentElement.lastElementChild.focus();
+        } else if (nodeElement.previousElementSibling.disabled === true) {
+          nodeElement.previousElementSibling.previousElementSibling.focus();
+        } else {
+          nodeElement.previousElementSibling.focus();
+        }
+        e.preventDefault();
+        break;
+      }
+      default:
+        break;
+    }
+  };
 function ContentSwitcher({ activeIndex, onChange, children }) {
     const [activeSwitch, changeSwitch] = useState(activeIndex);
 
@@ -12,7 +43,8 @@ function ContentSwitcher({ activeIndex, onChange, children }) {
                 changeSwitch(index);
                 onChange(Object.assign({}, e, { label, switchIndex: index }))
             },
-            active: (activeSwitch === index)
+            active: (activeSwitch === index),
+            onKeyDown : keyDownOnContextSwitch
         });
     });
 
