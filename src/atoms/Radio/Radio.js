ff8070471702b266d5797abad7d9ca4ff18b7c24
Delita <delitajosna.dsouza@hcl.com>
2020-06-09 17:37:25 +0530

    RadioGroup is added

--- src/atoms/Radio/Radio.js
+++ src/atoms/Radio/Radio.js
@@ -2,55 +2,54 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import prefix from '../../settings';
 
-const Radio = ({ className, labelText, ...restProps }) => {
-    const classnames = `${prefix}-radio-item ${className}`.trim();
+const Radio = ({ className, labelText, onChange, ...restProps }) => {
+  const classnames = `${prefix}-radio-item ${className}`.trim();
 
-    return (
-        <div className={classnames}>
-            <input
-                className={`${prefix}-radio`}
-                type="radio"
-                {...restProps}
-                onChange={event => {
-                    if (restProps.onChange) {
-                        restProps.onChange(event);
-                    }
-                }}
-            />
-            {labelText ? (
-                <label className={`${prefix}-radio-label`} htmlFor={restProps.id}>
-                    {labelText}
-                </label>
-            ) : null}
-        </div>
-    );
-}
+  const handleChange = (evt) => {
+    onChange(evt.target.value, evt);
+  };
+  return (
+    <div className={classnames}>
+      <input
+        className={`${prefix}-radio`}
+        type="radio"
+        {...restProps}
+        onChange={handleChange}
+      />
+      {labelText ? (
+        <label className={`${prefix}-radio-label`} htmlFor={restProps.id}>
+          {labelText}
+        </label>
+      ) : null}
+    </div>
+  );
+};
 
 Radio.propTypes = {
-    /** Custom class on the Radio wrapper. */
-    className: PropTypes.string,
-    /** Disable Radio */
-    disabled: PropTypes.bool,
-    /** Text for Radio Label. */
-    labelText: PropTypes.string,
-    /** Unique identifier of the element. */
-    id: PropTypes.string,
-    /** Accepts event handler as prop/argument. */
-    onChange: PropTypes.func,
-    /** Control Checked state for Radio */
-    checked: PropTypes.bool,
-    /** Name of radio element */
-    name: PropTypes.string,
-    /** Text value to be considered when submitting forms. */
-    value: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
+  /** Custom class on the Radio wrapper. */
+  className: PropTypes.string,
+  /** Disable Radio */
+  disabled: PropTypes.bool,
+  /** Text for Radio Label. */
+  labelText: PropTypes.string,
+  /** Unique identifier of the element. */
+  id: PropTypes.string,
+  /** Accepts event handler as prop/argument. */
+  onChange: PropTypes.func,
+  /** Control Checked state for Radio */
+  checked: PropTypes.bool,
+  /** Name of radio element */
+  name: PropTypes.string,
+  /** Text value to be considered when submitting forms. */
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 Radio.defaultProps = {
-    className: '',
-    labelText: '',
-    disabled: false,
-    onChange: () => { },
-    checked: false
+  className: '',
+  labelText: '',
+  disabled: false,
+  onChange: () => {},
+  checked: false,
 };
 
 export default Radio;
--- src/atoms/Radio/radio.stories.js
+++ src/atoms/Radio/radio.stories.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
 import { text, boolean } from '@storybook/addon-knobs';
 //@update-path-build-start
 import Radio from './Radio';
@@ -15,8 +14,8 @@ storiesOf('RadioButton', module)
         id="radio1"
         labelText={text('Label', 'Radio Label')}
         name="test"
-        onChange={action(event)}
         value={text('Value', 'standard')}
+        checked
       />
     ),
     {
@@ -27,24 +26,3 @@ storiesOf('RadioButton', module)
       }
     }
   )
-  .add(
-    'checked',
-    () => (
-      <Radio
-        checked
-        disabled={boolean('Disabled', false)}
-        id="radio1"
-        labelText={text('Label', 'Radio Label')}
-        name="test"
-        onChange={action(event)}
-        value={text('Value', 'standard')}
-      />
-    ),
-    {
-      info: {
-        text: `Description About Radio Component \n
-
-        import { Radio } from '@patron/patron-react/radio'`
-      }
-    }
-  );
