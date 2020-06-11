fae546cbc71b72b20df915ee1902286fb3961576
Vivek Ananth <ananthv@hcl.com>
2020-06-11 18:54:56 +0530

    checkbox storybook changes

--- src/atoms/Checkbox/checkbox.stories.js
+++ src/atoms/Checkbox/checkbox.stories.js
@@ -7,42 +7,78 @@ import Checkbox from './Checkbox';
 //@update-path-build-end
 
 storiesOf('Checkbox', module)
-  .add(
-    'default',
-    () => (
-      <Checkbox
-        id="checkbox1"
-        value={text('Value', 'standard')}
-        label={text('Label', 'Checkbox Label')}
-        disabled={boolean('Disabled', false)}
-        onChange={action(event)}
-      />
-    ),
-    {
-      info: {
-        text: `Description About Checkbox Component \n
+    .add(
+        'default',
+        () => (
+            <Checkbox
+                id="checkbox1"
+                value={text('Value', 'standard')}
+                label={text('Label', 'Checkbox Label')}
+                disabled={boolean('Disabled', false)}
+                onChange={action(event)}
+            />
+        ),
+        {
+            info: {
+                text: `Description About Checkbox Component \n
 
       import { Checkbox } from '@patron/patron-react/checkbox'`
-      }
-    }
-  )
-  .add(
-    'checked',
-    () => (
-      <Checkbox
-        id="checkbox1"
-        checked
-        value={text('Value', 'standard')}
-        label={text('Label', 'Checkbox Label')}
-        disabled={boolean('Disabled', false)}
-        onChange={action(event)}
-      />
-    ),
-    {
-      info: {
-        text: `Description About Checkbox Component \n
+            }
+        }
+    )
+    .add(
+        'horizontal',
+        () => (
+            <div className="hcl-checkbox-group">
+                <Checkbox
+                    id="checkbox1"
+                    value={text('Value', 'standard')}
+                    label={text('Label', 'Checkbox Label 1')}
+                    onChange={action(event)}
+                    disabled={boolean("disabled", false)}
+                />
+                <Checkbox
+                    id="checkbox2"
+                    label={'Checkbox Label 2'}
+                    checked
+                    onChange={action(event)}
+                />
+                <Checkbox id="checkbox3" label="Checkbox Label 3 (disabled)" disabled />
+            </div>
+        ),
+        {
+            info: {
+                text: `Description About Checkbox Component \n
   
         import { Checkbox } from '@patron/patron-react/checkbox'`
-      }
-    }
-  );
+            }
+        }
+    )
+    .add(
+        'vertical',
+        () => (
+            <div className="hcl-checkbox-group hcl-stack-vertical">
+                <Checkbox
+                    id="checkbox1"
+                    value={text('Value', 'standard')}
+                    label={text('Label', 'Checkbox Label 1')}
+                    onChange={action(event)}
+                    disabled={boolean("disabled", false)}
+                />
+                <Checkbox
+                    id="checkbox2"
+                    label={'Checkbox Label 2'}
+                    checked
+                    onChange={action(event)}
+                />
+                <Checkbox id="checkbox3" label="Checkbox Label 3 (disabled)" disabled />
+            </div>
+        ),
+        {
+            info: {
+                text: `Description About Checkbox Component \n
+  
+        import { Checkbox } from '@patron/patron-react/checkbox'`
+            }
+        }
+    );
