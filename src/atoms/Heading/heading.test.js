83a10f6afd493c4a1e0385599cb5fd9c38e2f319
srinivasan-a <[Your>
2020-11-16 16:31:21 +0530

    added test for header component

---
+++ src/atoms/Heading/heading.test.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import Heading from './Heading'
+import { mount } from 'enzyme';
+import { exportAllDeclaration } from '@babel/types';
+
+test("can provide a label",()=>{
+    const wrapper = mount(<Heading type="h1">Patronus</Heading>) 
+    expect(wrapper.text()).toBe('Patronus')
+})
+test("can provide custom class name",()=>{
+    const wrapper = mount(<Heading type="h1" className="patronus-header">Patronus</Heading>)
+    expect(wrapper.find(Heading).hasClass('patronus-header')).toBe(true);
+})
+test("render heading with h1 type", ()=>{
+    const wrapper = mount(<Heading type="h1">Heading</Heading>)
+    expect(wrapper.find('.hcl-h1').length).toBe(1)
+})
+test("render heading with h2 type", ()=>{
+    const wrapper = mount(<Heading type="h2">Heading</Heading>)
+    expect(wrapper.find('.hcl-h2').length).toBe(1)
+})
+test("render heading with h3 type", ()=>{
+    const wrapper = mount(<Heading type="h3">Heading</Heading>)
+   expect(wrapper.find('.hcl-h3').length).toBe(1)
+})
+test("render heading with h4 type", ()=>{
+    const wrapper = mount(<Heading type="h4">Heading</Heading>)
+    expect(wrapper.find('.hcl-h4').length).toBe(1)
+})
+test("render heading with h5 type", ()=>{
+    const wrapper = mount(<Heading type="h5">Heading</Heading>)
+    expect(wrapper.find('.hcl-h5').length).toBe(1)
+})
+test("render heading with h6 type", ()=>{
+    const wrapper = mount(<Heading type="h6">Heading</Heading>)
+    expect(wrapper.find('.hcl-h6').length).toBe(1)
+})
