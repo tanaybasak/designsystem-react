d6b717a1b66e6474955fc138fbb0574bf21a8f9c
Delita <delitajosna.dsouza@hcl.com>
2020-12-16 17:16:10 +0530

    button changes in stories

--- src/atoms/Button/button.stories.js
+++ src/atoms/Button/button.stories.js
@@ -5,6 +5,7 @@ import { select, text, boolean } from '@storybook/addon-knobs';
 //@update-path-build-start
 import Button from './Button';
 //@update-path-build-end
+import icons from '../../../.storybook/iconList';
 
 const classOptions = [
   'primary',
@@ -18,27 +19,85 @@ const classOptions = [
 
 const typeOptions = ['button', 'reset', 'submit'];
 
-storiesOf('Button', module).add(
-  'default',
-  () => (
-    <Button
-      type={select('Type', classOptions, 'primary')}
-      kind={select('Kind', typeOptions, '')}
-      title="Default"
-      disabled={boolean('Disabled', false)}
-      small={boolean('Small', false)}
-      onClick={action('button-click')}
-    >
-      {text('Label', 'Click Me')}
-    </Button>
-  ),
-  {
-    info: {
-      text: `Description About Button Component \n
+storiesOf('Button', module)
+  .add(
+    'default',
+    () => (
+      <Button
+        type={select('Type', classOptions, 'primary')}
+        kind={select('Kind', typeOptions, 'button')}
+        title="Default"
+        disabled={boolean('Disabled', false)}
+        small={boolean('Small', false)}
+        onClick={action('button-click')}
+      >
+        {text('Label', 'Click Me')}
+      </Button>
+    ),
+    {
+      info: {
+        text: `Description About Button Component \n
 
     import { Button } from '@patron/patron-react/button';
       
       `
+      }
     }
-  }
-);
+  )
+  .add(
+    'with icon',
+    () => (
+      <Button
+        type={select('Type', classOptions, 'primary')}
+        kind={select('Kind', typeOptions, 'button')}
+        title="Default"
+        disabled={boolean('Disabled', false)}
+        small={boolean('Small', false)}
+        onClick={action('button-click')}
+      >
+        <i
+          className={`p-hclsw p-hclsw-${select('Icon Class', icons, 'user')}`}
+        />
+      </Button>
+    ),
+    {
+      info: {
+        text: `Description About Button Component \n
+
+    import { Button } from '@patron/patron-react/button';
+      
+      `
+      }
+    }
+  )
+  .add(
+    'with text and icon',
+    () => (
+      <Button
+        type={select('Type', classOptions, 'primary')}
+        kind={select('Kind', typeOptions, 'button')}
+        title="Default"
+        disabled={boolean('Disabled', false)}
+        small={boolean('Small', false)}
+        onClick={action('button-click')}
+      >
+        {text('Label', 'Click Me')}
+        <i
+          className={`hcl-btn-icon p-hclsw p-hclsw-${select(
+            'Icon Class',
+            icons,
+            'user'
+          )}`}
+        />
+      </Button>
+    ),
+    {
+      info: {
+        text: `Description About Button Component \n
+
+    import { Button } from '@patron/patron-react/button';
+      
+      `
+      }
+    }
+  );
