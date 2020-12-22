47de1ce3796309a5cfd9d697015c3545bafbeb1e
Tanoy Basak <tanoybasak@Tanoys-MacBook-Pro.local>
2020-12-22 14:45:04 +0530

    new change

--- src/atoms/ProgressBar/CircleProgressBar.js
+++ src/atoms/ProgressBar/CircleProgressBar.js
@@ -10,164 +10,61 @@ const CircleProgressBar = ({
   customContent
 }) => {
   const [offset, setOffset] = useState(0);
-  const [topelement, setTopElement] = useState(null);
-  const [bottomelement, setBottomElement] = useState(null);
-  const [center, setCenter] = useState(0);
-  const [radius, setRadius] = useState(0);
-  const [circumference, setCircumference] = useState(0);
   const [size, setSize] = useState(48);
   const svgRef = useRef(null);
   const circleRef = useRef(null);
-  const circleContainer = useRef(null);
-  const svgContainer = useRef(null);
 
   progress = progress > 1 ? 1 : progress;
   const prg = progress * 100;
-  const progressElement = React.createElement(
-    'span',
-    { className: `${prefix}-progressbar-circle-text` },
-    customContent
-  );
-  const progresslabel = React.createElement(
-    'span',
-    { className: `${prefix}-progressbar-circle-text` },
-    label
-  );
-  const breakLine = React.createElement('br', null, null);
+  const circumference = 2 * Math.PI * 20;
+  const progressOffset = ((100 - prg) / 100) * circumference;
+
   useEffect(() => {
-    if (svgRef.current) {
-      setSize(svgRef.current.clientHeight);
-    }
-    setCenter(size / 2);
-    setRadius(size / 2 - 2.5);
-    setCircumference(2 * Math.PI * (size / 2 - 2.5));
-    const progressOffset = ((100 - prg) / 100) * circumference;
+    setSize(svgRef.current.clientHeight);
     setOffset(progressOffset);
-    if (circleRef.current)
-      circleRef.current.style =
-        'transition: stroke-dashoffset 850ms ease-in-out';
-    if (labelPosition == 'left' && (size == 48 || size == 96)) {
-      setBottomElement(null);
-      circleContainer.current.classList.remove(`${prefix}-pb-top-bottom`);
-      setTopElement(
-        React.createElement(
-          'div',
-          {
-            className: `mr-1 ${prefix}-progressbar-circle-text ${prefix}-pb-displayLeft`
-          },
-          label
-        )
-      );
-    } else if (labelPosition == 'right' && (size == 48 || size == 96)) {
-      setTopElement(null);
-      circleContainer.current.classList.remove(`${prefix}-pb-top-bottom`);
-      setBottomElement(
-        React.createElement(
-          'div',
-          { className: `ml-1 ${prefix}-progressbar-circle-text` },
-          label
-        )
-      );
-    } else if (labelPosition == 'bottom' && (size == 48 || size == 96)) {
-      setTopElement(null);
-      circleContainer.current.classList.add(`${prefix}-pb-top-bottom`);
-      setBottomElement(
-        React.createElement(
-          'div',
-          { className: `mt-1 ${prefix}-progressbar-circle-text` },
-          label
-        )
-      );
-    } else if (labelPosition == 'top' && (size == 48 || size == 96)) {
-      setBottomElement(null);
-      circleContainer.current.classList.add(`${prefix}-pb-top-bottom`);
-      setTopElement(
-        React.createElement(
-          'div',
-          { className: `mb-1 ${prefix}-progressbar-circle-text` },
-          label
-        )
-      );
-    } else if (
-      (labelPosition == 'left' || labelPosition == 'bottom') &&
-      size == 16
-    ) {
-      setBottomElement(null);
-      circleContainer.current.classList.remove(`${prefix}-pb-top-bottom`);
-      setTopElement(
-        React.createElement(
-          'div',
-          { className: `mr-1 ${prefix}-progressbar-circle-text` },
-          [progresslabel, breakLine, progressElement]
-        )
-      );
-    } else if (
-      (labelPosition == 'right' || labelPosition == 'top') &&
-      size == 16
-    ) {
-      setTopElement(null);
-      circleContainer.current.classList.remove(`${prefix}-pb-top-bottom`);
-      setBottomElement(
-        React.createElement(
-          'div',
-          { className: `ml-1 ${prefix}-progressbar-circle-text` },
-          [progresslabel, breakLine, progressElement]
-        )
-      );
-    }
-  }, [
-    setOffset,
-    setBottomElement,
-    setTopElement,
-    progress,
-    circumference,
-    offset,
-    label,
-    labelPosition,
-    customContent
-  ]);
+  }, [setSize, progress, label, labelPosition, customContent]);
   return (
     <>
       {type == 'determinate' ? (
-        <div className={`${prefix}-circle-pb`} ref={circleContainer}>
-          {label.length && type == 'determinate' ? topelement : null}
-          <div
-            className={`${prefix}-determ-pb-circle-small`}
-            ref={svgContainer}
-          >
+        <div
+          className={`${prefix}-pb-circle-wrapper ${prefix}-pb-label-leftRight`}
+        >
+          <div className={`${prefix}-pb-circle-large`}>
             <svg
-              className={`${prefix}-progressbar-circle`}
-              width={size}
-              height={size}
+              className={`${prefix}-pb-circle-svg-determ`}
               ref={svgRef}
+              viewBox="25 25 50 50"
             >
               <circle
-                className={`${prefix}-progressbar-circle-inner`}
-                cx={center}
-                cy={center}
-                r={radius}
+                className={`${prefix}-pb-bgcircle pb-circle-inner`}
+                ref={circleRef}
+                cx="50"
+                cy="50"
+                r="20"
               />
               <circle
-                className={`${prefix}-progressbar-circle-outer`}
-                ref={circleRef}
-                cx={center}
-                cy={center}
-                r={radius}
-                strokeDasharray={circumference}
+                className={`pb-circle-outer`}
+                cx="50"
+                cy="50"
+                r="20"
                 strokeDashoffset={offset}
+                strokeDasharray={circumference}
               />
             </svg>
             {size == 16 ? null : (
-              <div className={`${prefix}-progressbar-circle-customContent`}>
-                {customContent}
-              </div>
+              <div className={`${prefix}-pb-circle-text`}>{customContent}</div>
             )}
           </div>
-          {label.length && type == 'determinate' ? bottomelement : null}
+          <div className={`${prefix}-pb-label-content`}>
+            <div className={`${prefix}-pb-label-text`}>{label}</div>
+          </div>
         </div>
       ) : (
-        <div className={`${prefix}-indeterm-pb-circle`}>
-          <svg className={`${prefix}-circle-pb-svg`} viewBox="25 25 50 50">
+        <div className={`${prefix}-pb-circle`}>
+          <svg
+            className={`${prefix}-pb-circle-svg-indeterm`}
+            viewBox="25 25 50 50"
+          >
             <circle
               className={`${prefix}-pb-bgcircle pb-circle`}
               cx="50"
--- src/atoms/ProgressBar/LinearProgressBar.js
+++ src/atoms/ProgressBar/LinearProgressBar.js
@@ -10,107 +10,91 @@ const LinearProgressBar = ({
   customContent
 }) => {
   const [finalVal, updateFinalValue] = useState(0);
-
-  const progressStyle = {
-    width: finalVal,
-    height: '100%',
-    transition: 'width 1s ease-in-out'
-  };
-
   useEffect(() => {
     progress = progress > 1 ? 1 : progress;
     updateFinalValue(progress * 100 + '%');
   }, [updateFinalValue, progress, label, subText, type, customContent]);
   return type === 'determinate' ? (
-    <div className={`${prefix}-progressbar-linear`}>
-      <div className={`${prefix}-progressbar-linear-label mb-2`}>
-        <span className={`${prefix}-progressbar-linear-label-content`}>
-          {label}
-        </span>
-        <span className={`${prefix}-progressbar-linear-label-value`}>
+    <div className={`${prefix}-pb-linear-wrapper`} id="pbar-linear-determ">
+      <div className={`${prefix}-pb-linear-label`}>
+        <span className={`${prefix}-pb-linear-label-content`}>{label}</span>
+        <span className={`${prefix}-pb-linear-label-value`}>
           {customContent}
         </span>
       </div>
-      <div className={`${prefix}-progressbar-linear-content`}>
-        <div
-          className={`${prefix}-progressbar-linear-style`}
-          style={progressStyle}
+      <svg
+        className={`${prefix}-pb-linear`}
+        preserveAspectRatio="none"
+        viewBox="0 0 100 4"
+      >
+        <line
+          className={`${prefix}-pb-linear-bgline hcl-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="100"
+          y2="2"
         />
-      </div>
-      {subText ? (
-        <div className={`${prefix}-progressbar-linear-subtext mt-2`}>
-          {' '}
-          {subText}{' '}
-        </div>
-      ) : (
-        ''
-      )}
+        <line
+          className={`${prefix}-pb-linear-mainline hcl-pb-linear-line`}
+          x1={finalVal}
+          y1="2"
+          x2="0"
+          y2="2"
+        />
+      </svg>
+      <div className={`${prefix}-pb-linear-subtext mt-2`}>Subtext</div>
     </div>
   ) : (
-    <>
-      <div className={`${prefix}-progressbar-linear-label`}>
-        <span className={`${prefix}-progressbar-linear-label-content`}>
-          {label}
-        </span>
-      </div>
-      <div className={`${prefix}-indeterm-pb-linear`}>
-        <svg
-          className={`${prefix}-linear`}
-          preserveAspectRatio="none"
-          viewBox="0 0 125 4"
-        >
-          <line
-            className={`${prefix}-linear-bgline ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-          <line
-            className={`${prefix}-linear-mainline-one ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-          <line
-            className={`${prefix}-linear-mainline-two ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-          <line
-            className={`${prefix}-linear-dot ${prefix}-linear-dot-one ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-          <line
-            className={`${prefix}-linear-dot ${prefix}-linear-dot-two ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-          <line
-            className={`${prefix}-linear-dot ${prefix}-linear-dot-three ${prefix}-linear-line`}
-            x1="0"
-            y1="2"
-            x2="125"
-            y2="2"
-          />
-        </svg>
-      </div>
-      {subText ? (
-        <div className={`${prefix}-progressbar-linear-subtext mt-2`}>
-          {subText}
-        </div>
-      ) : (
-        ''
-      )}
-    </>
+    <div className={`${prefix}-pb-linear-wrapper`}>
+      <svg
+        className={`${prefix}-pb-linear`}
+        preserveAspectRatio="none"
+        viewBox="0 0 125 4"
+      >
+        <line
+          className={`${prefix}-pb-linear-bgline ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+        <line
+          className={`${prefix}-pb-linear-mainline-one ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+        <line
+          className={`${prefix}-pb-linear-mainline-two ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+        <line
+          className={`${prefix}-pb-linear-dot ${prefix}-pb-linear-dot-one ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+        <line
+          className={`${prefix}-pb-linear-dot ${prefix}-pb-linear-dot-two ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+        <line
+          className={`${prefix}-pb-linear-dot ${prefix}-pb-linear-dot-three ${prefix}-pb-linear-line`}
+          x1="0"
+          y1="2"
+          x2="125"
+          y2="2"
+        />
+      </svg>
+    </div>
   );
 };
 
