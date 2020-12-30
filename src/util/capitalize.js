e7485a62add881e41427834ab66e4a4e19633128
Tanoy Basak <tanoybasak@Tanoys-MacBook-Pro.local>
2020-12-30 22:10:16 +0530

    new chnages timeline

--- src/util/capitalize.js
+++ src/util/capitalize.js
@@ -1,5 +1,4 @@
 const capitalizeFirstLetter = (str, partition = '-') => {
-  console.log(str);
   str = str.split(partition);
   for (var i = 0, x = str.length; i < x; i++) {
     str[i] = str[i][0].trim().toUpperCase() + str[i].substr(1);
