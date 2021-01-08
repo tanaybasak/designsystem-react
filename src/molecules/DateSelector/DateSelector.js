2f11088108ee6ed62a83799f5152a84b26b301bf
Vivek Vijayan <vijayanv@hcl.com>
2021-01-08 15:06:18 +0530

    Update DateSelector.js

--- src/molecules/DateSelector/DateSelector.js
+++ src/molecules/DateSelector/DateSelector.js
@@ -219,7 +219,7 @@ DateSelector.propTypes = {
   minDate: PropTypes.instanceOf(Date),
   /** Max date */
   maxDate: PropTypes.instanceOf(Date),
-  /** Callback on Calender toggle */
+  /** Callback on Calendar toggle */
   onVisibleChange: PropTypes.func
 };
 
