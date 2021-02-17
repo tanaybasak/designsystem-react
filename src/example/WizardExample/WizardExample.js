3f5ba148c61f5ea8cf783f64307eae1556e81689
vivek <vivek.ananth87@gmail.com>
2021-02-17 21:50:41 +0530

    completed css missed

--- src/example/WizardExample/WizardExample.js
+++ src/example/WizardExample/WizardExample.js
@@ -1,9 +1,7 @@
 /* eslint-disable no-console */
 import React, { Component } from 'react';
 import { Wizard, Step } from '../../molecules/Wizard';
-// import { User, User2 } from '../../util/icons';
 import Button from '../../atoms/Button';
-import { Radio } from '../../atoms/radio';
 import TextInput from '../../atoms/TextInput';
 import FormHelperText from '../../atoms/FormHelperText';
 import Label from '../../atoms/Label';
@@ -14,7 +12,6 @@ import Tile from '../../atoms/Tile';
 class WizardExample extends Component {
   constructor(props) {
     super(props);
-    // this.rr = React.createRef();
   }
 
   state = {
@@ -58,21 +55,9 @@ class WizardExample extends Component {
   };
 
   handleStepClick = (idx, e) => {
-    // debugger;
     if (this.state.selIndex === idx) {
       return;
     }
-    // if (this.lastStepCompleted() > this.state.selIndex) {
-    //   return;
-    // }
-    // if (idx > this.state.selIndex) {
-    // this.handleNext();
-    // } else {
-    // this.handleBack();
-    // }
-    //step by step
-    // if (this.state.selIndex - 1 === idx || this.state.selIndex + 1 === idx)
-    //   this.goToStep(idx);
     this.goToStep(idx);
   };
 
@@ -145,7 +130,7 @@ class WizardExample extends Component {
       case 0:
         return (
           <>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="First Name"
@@ -158,7 +143,7 @@ class WizardExample extends Component {
               />
               <Label htmlFor="firstname">First Name</Label>
             </div>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="Last Name"
@@ -176,7 +161,7 @@ class WizardExample extends Component {
       case 1:
         return (
           <>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <FileUploader
                 className="hcl-btn hcl-primary hcl-sm"
                 description="Supports multiple Uploads"
@@ -195,7 +180,7 @@ class WizardExample extends Component {
       case 2:
         return (
           <>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextArea
                 aria-label="comments"
                 placeholder="Social Markup text"
@@ -207,7 +192,7 @@ class WizardExample extends Component {
       case 3:
         return (
           <>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="Campaign URL"
@@ -224,7 +209,7 @@ class WizardExample extends Component {
                 Campaign URL generated from Campaign URL Builder
               </div>
             </div>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="Campaign Source"
@@ -245,7 +230,7 @@ class WizardExample extends Component {
       case 4:
         return (
           <>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="Firt Name"
@@ -261,7 +246,7 @@ class WizardExample extends Component {
                 Enter First Name
               </FormHelperText>
             </div>
-            <div className="hcl-col-3 hcl-form-group">
+            <div className="hcl-col-md-3 hcl-col-sm-12 hcl-form-group">
               <TextInput
                 type="text"
                 placeholder="Last Name"
@@ -299,7 +284,7 @@ class WizardExample extends Component {
       <div className="hcl-container">
         <div className="hcl-row">
           <Wizard
-            kind={'style1'}
+            kind={'style2'}
             // iconType="noicon"
             currentStepLabel={
               this.state.selIndex != null &&
@@ -320,22 +305,6 @@ class WizardExample extends Component {
                   title={item.title}
                   description={item.description}
                   status={item.status}
-                  // activeIcon={
-                  //   <img
-                  //     style={{ width: '100%', height: '100%' }}
-                  //     src={
-                  //       'https://png.pngtree.com/element_our/png/20181011/instagram-social-media-icon-design-template-vector-png_126996.jpg'
-                  //     }
-                  //   />
-                  // }
-                  // defaultIcon={
-                  //   <img
-                  //     style={{ width: '100%', height: '100%' }}
-                  //     src={
-                  //       'https://img.icons8.com/plasticine/2x/google-logo.png'
-                  //     }
-                  //   />
-                  // }
                   onClick={this.handleStepClick.bind(this, idx)}
                 />
               );
@@ -378,8 +347,6 @@ class WizardExample extends Component {
               style={{
                 marginLeft: '30px'
               }}
-              // ref={this.finishRef}
-              //   disabled={this.state.selIndex === 5}
               onClick={this.handleFinish.bind(this)}
             >
               Finish
@@ -392,7 +359,6 @@ class WizardExample extends Component {
                 display: this.isAllStepsCompleted() ? 'none' : 'unset',
                 marginLeft: '30px'
               }}
-              // ref={this.nextRef}
               onClick={this.handleNext.bind(this)}
             >
               Next
