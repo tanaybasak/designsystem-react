823f0536480494295e3f3d9ee3eb29929f9cb180
Delita <delitajosna.dsouza@hcl.com>
2021-02-25 14:23:55 +0530

    unlink icon is added

--- src/util/icons.js
+++ src/util/icons.js
@@ -331,7 +331,7 @@ export const unlinkIcon = (
   <svg
     id="icon"
     className="hcl-btn-icon"
-    xmlns="http://www.w3.org/2000/svg"
+    xmlns="https://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
   >
     <rect
