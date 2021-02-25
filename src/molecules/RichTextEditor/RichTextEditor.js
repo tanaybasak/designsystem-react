d2316377dac7c24f72167abab83cd8136f9102c4
Delita <delitajosna.dsouza@hcl.com>
2021-02-25 10:55:12 +0530

    validate URL in RTE

--- src/molecules/RichTextEditor/RichTextEditor.js
+++ src/molecules/RichTextEditor/RichTextEditor.js
@@ -210,7 +210,7 @@ const RichTextEditor = ({
 
   const isUrlValid = userInput => {
     var res = userInput.match(
-      /(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g
+      /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#[\]@!$&'()*+,;=.]+$/
     );
     if (res == null) return false;
     else return true;
