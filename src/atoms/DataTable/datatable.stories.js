00a0732d108cfacc0d62358bb0f41d4785965ed5
Tanoy Basak <tanoybasak@Tanoys-MacBook-Pro.local>
2021-03-19 22:49:29 +0530

    changes

--- src/atoms/DataTable/datatable.stories.js
+++ src/atoms/DataTable/datatable.stories.js
@@ -39,6 +39,69 @@ const tableData = [
     rule: 'Round Robin',
     attachedGroups: 'Maureen VM Groups',
     status: 'Active'
+  },
+  {
+    id: 4,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 5,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 6,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 7,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 8,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 9,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
+  },
+  {
+    id: 10,
+    name: 'Load Balancer 3',
+    protocol: 'HTTP',
+    port: '80',
+    rule: 'Round Robin',
+    attachedGroups: 'Maureen VM Groups',
+    status: 'Active'
   }
 ];
 
@@ -501,7 +564,7 @@ storiesOf('DataTable', module)
         headerSelection={
           <Checkbox aria-label="header checkbox" id={`header_checkbox`} />
         }
-        resizer
+        resizer={{ handle: 'right' }}
         onColumnAfterResize={action('column-resized')}
       />
     ),
