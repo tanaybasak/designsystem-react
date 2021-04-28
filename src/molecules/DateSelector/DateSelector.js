27f2d8f10f25682923e871d4df76d0c31fea9d9d
Vivek Vijayan <vivekvijayan@Viveks-MacBook-Pro.local>
2021-04-28 18:58:27 +0530

    date selector issue resolved

--- src/molecules/DateSelector/DateSelector.js
+++ src/molecules/DateSelector/DateSelector.js
@@ -1,7 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import {
-  createDateObj,
   convertToDateObj,
   convertToDateString
 } from '../../util/utility';
